e2e7f552830aed40b7cd55d376e156a78dbe399b
Juho <juho.keskinen@seamk.fi.007@gmail.com>
2025-04-02 15:40:33 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import os
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO
+import sqlite3 #tietokantakirjasto
+import eventlet
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='eventlet')
 
-mittaukset = dict()
+#tietokannan teko
+yhteys = sqlite3.connect('mittaukset.db3')
+kursori = yhteys.cursor()
+kursori.execute("CREATE TABLE IF NOT EXISTS mittaukset (id INTEGER PRIMARY KEY, paiva TEXT, mittaus INTEGER)")
+yhteys.commit()
+yhteys.close()
+
+mittaukset = dict() # {'maanantai' : 7}
 
 @app.route('/', methods=['GET'])
 def index():
@@ -14,11 +23,17 @@ def index():
 @app.route('/lisaa_tieto', methods=['POST'])
 def lisaa_tieto():
     data = request.get_json(force=True)
-    if 'mittaus' in data and isinstance(data['mittaus'], list) and len(data['mittaus']) == 2:
-        mittaukset[data['mittaus'][0]] = data['mittaus'][1]
-        socketio.emit('data_update')
-        return "200"
-    return "400 Bad Request", 400
+    mittaustulos_lista = data['mittaus']
+    mittaukset[mittaustulos_lista[0]] = mittaustulos_lista[1]
+    yhteys = sqlite3.connect('mittaukset.db3')
+    kursori = yhteys.cursor()
+    kursori.execute("INSERT INTO mittaukset (paiva, mittaus) VALUES (?, ?)", (mittaustulos_lista[0], mittaustulos_lista[1]))
+    yhteys.commit()
+    yhteys.close()
+
+    print("mittaustulos:", mittaustulos_lista)
+    socketio.emit('data_update')
+    return "200"
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host="0.0.0.0")
+    socketio.run(app, debug=True)
